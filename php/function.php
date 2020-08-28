944e1b58c17aeb40fe769f9daf0e77de4707fa7c
izza_masss <48864843+izzamasss@users.noreply.github.com>
2020-08-29 05:33:16 +0700

    Update function.php

--- php/function.php
+++ php/function.php
@@ -14,9 +14,6 @@
 		global $conn;
 		$username = strtolower(htmlspecialchars($data["username"])); //membersihkan input an backslash kemudian smeua input di ubah sebagai char
 		$password = mysqli_real_escape_string($conn,$data["password"]); //memungkinkan " sebagai input an
-		// enkripsi password
-		$password = password_hash($password, PASSWORD_DEFAULT);
-
 		mysqli_query($conn,"INSERT INTO tb_user VALUES ('','$username','$password')");
 	}
  ?>
